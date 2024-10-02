9e7dbcfd1077452b495691bf62a962fcf61daa06
TheKingXion <luis.gonzalez.oyarce@alumnos.cmch.maristas.cl>
2024-10-02 15:31:35 -0300

    Hola

---
+++ pruebas.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir "AutomaticoGIt"
+git init
+touch "AutomaticoGIt/README.md"
+read -p "Ingresa el mensaje del commit: " mensaje
+git add .
+git commit -m "$mensaje"
+
+echo "Cambios subidos con éxito."
